98a4078eb1fb81e4d4225f70e1bf4f5a7469182c
larissafzl <fazolinlarissa@gmail.com>
2024-09-19 08:48:40 -0300

    add README file

---
+++ Earth_Final_Season/Earth_Final_Season/Earth_Final_SeasonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Earth_Final_SeasonApp.swift
+//  Earth_Final_Season
+//
+//  Created by Larissa Fazolin on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Earth_Final_SeasonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
