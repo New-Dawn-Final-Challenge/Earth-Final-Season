5927b45929bcbad3b8c432d8bc966ad03f47fe07
Breno Harris <80071445+Breno-VH@users.noreply.github.com>
2024-09-24 10:10:46 -0300

    primeira versao dos protocolos

---
+++ Earth_Final_Season/Earth_Final_Season/Models/DifficultyEnum.swift
@@ -0,0 +1,12 @@
+//
+//  DIficultyEnum.swift
+//  Earth_Final_Season
+//
+//  Created by Breno Harris on 24/09/24.
+//
+
+import Foundation
+
+public enum Difficulty {
+    case easy, medium, hard
+}
