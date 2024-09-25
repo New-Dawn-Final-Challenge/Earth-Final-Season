78671a36078bbaae2dbe8bbe13405e6754d1651b
larissafzl <fazolinlarissa@gmail.com>
2024-09-25 11:30:49 -0300

    add gameplay viewModel logic

--- Earth_Final_Season/Earth_Final_Season/ViewModels/GameplayViewModel.swift
+++ Earth_Final_Season/Earth_Final_Season/ViewModels/GameplayViewModel.swift
@@ -5,3 +5,30 @@
 //  Created by Larissa Fazolin on 19/09/24.
 //
 
+import Foundation
+
+class GameplayViewModel: ObservableObject {
+    @Published var currentEvent: Event?
+    @Published var indicators = Indicators(audience: 5, socialInstability: 5, politicalInstability: 5, environmentalDegradation: 5, currentYear: 0)  // Initial Indicators
+    
+    private var events: [UUID: Event]
+    private var eventsSequence: [UUID]
+
+    init(events: [Event]) {
+        self.events = Dictionary(uniqueKeysWithValues: events.map { ($0.id, $0) })
+        self.eventsSequence = events.map { $0.id }
+        currentEvent = events.first
+    }
+    
+    func swipeRight() {
+        if let event = currentEvent {
+            indicators.applyConsequence(event.consequence1)
+        }
+    }
+    
+    func swipeLeft() {
+        if let event = currentEvent {
+            indicators.applyConsequence(event.consequence2)
+        }
+    }
+}
