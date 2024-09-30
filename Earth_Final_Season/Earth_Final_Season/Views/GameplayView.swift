dbac81879436463a216e70275bf61a1dfaa71aa2
larissafzl <fazolinlarissa@gmail.com>
2024-09-30 11:00:57 -0300

    organize GameplayView components

--- Earth_Final_Season/Earth_Final_Season/Views/GameplayView.swift
+++ Earth_Final_Season/Earth_Final_Season/Views/GameplayView.swift
@@ -4,53 +4,28 @@ struct GameplayView: View {
     @StateObject var viewModel = GameplayViewModel()
     
     var body: some View {
-        VStack(spacing: 10) {
-            HStack {
-                Spacer()
-                
-                NavigationLink(destination: ConfigurationsView(viewModel: ConfigurationsViewModel())) {
-                    HelpButtonView()
-                }
-                
-                NavigationLink(destination: ConfigurationsView(viewModel: ConfigurationsViewModel())) {
-                    ConfigurationButtonView()
-                }
-            }
-            .padding(.trailing)
+        VStack(spacing: 0) {
+            helperButtonsView
             
-            HStack(alignment: .center, spacing: 20) {
-                AudienceIndicatorView(percentage: viewModel.indicators.audience)
-                    .padding(.bottom)
-
-                ChaosIndicatorsView(
-                    socialInstability: viewModel.indicators.socialInstability,
-                    politicalInstability: viewModel.indicators.politicalInstability,
-                    environmentalDegradation: viewModel.indicators.environmentalDegradation,
-                    year: String(viewModel.indicators.currentYear)
-                )
-            }
+            indicatorsView
             
             if let event = viewModel.currentEvent {
+
+                CharacterView(characterImage: event.image, characterName: event.character)
                 
-                VStack(spacing: -18) {
-                    CharacterView(characterImage: event.image, characterName: event.character)
-                    
-                    EventView(mainScreenShadowRadius: $viewModel.mainScreenShadowRadius,
-                              eventDescription: event.description)
-                    
-                }
-                .padding(.bottom, -15)
-                
+                EventView(mainScreenShadowRadius: $viewModel.mainScreenShadowRadius,
+                          eventDescription: event.description)
+
                 VStack(spacing: -20) {
                     ChoicesView(shadowRadius: $viewModel.option1ShadowRadius,
                                  text: event.choice1)
                     .padding(.trailing, 100)
-                    
+
                     ChoicesView(shadowRadius: $viewModel.option2ShadowRadius,
                                  text: event.choice2)
                     .padding(.leading, 100)
                 }
-                
+                .padding(.top, -10)
             } else {
                 Text("No more events")
                     .font(.title)
@@ -72,6 +47,35 @@ struct GameplayView: View {
             Spacer()
         }
         .onAppear(perform: prepareHaptics)
+    }
+    
+    private var helperButtonsView: some View {
+        HStack {
+            Spacer()
+            
+            NavigationLink(destination: ConfigurationsView(viewModel: ConfigurationsViewModel())) {
+                HelpButtonView()
+            }
+            
+            NavigationLink(destination: ConfigurationsView(viewModel: ConfigurationsViewModel())) {
+                ConfigurationButtonView()
+            }
+        }
+        .padding(.trailing)
+    }
+    
+    private var indicatorsView: some View {
+        HStack(alignment: .center, spacing: 20) {
+            AudienceIndicatorView(percentage: viewModel.indicators.audience)
+                .padding(.bottom)
+
+            ChaosIndicatorsView(
+                socialInstability: viewModel.indicators.socialInstability,
+                politicalInstability: viewModel.indicators.politicalInstability,
+                environmentalDegradation: viewModel.indicators.environmentalDegradation,
+                year: String(viewModel.indicators.currentYear)
+            )
+        }
     }
 }
 
