d652c473a7ed3f19d252137bfd047556b18a2181
larissafzl <fazolinlarissa@gmail.com>
2024-10-18 09:02:29 -0300

    add missing binding

--- Earth_Final_Season/Earth_Final_Season/Views/MenuView.swift
+++ Earth_Final_Season/Earth_Final_Season/Views/MenuView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MenuView: View {
     @State var settingsVM = SettingsViewModel()
+    @State var leaderboardVM = LeaderboardViewModel()
     @State private var isGameCenterPresented = false
     
     var body: some View {
@@ -22,7 +23,8 @@ struct MenuView: View {
                     .cornerRadius(10)
                 
                 // Navigation buttons
-                NavigationLink(destination: GameplayView(settingsVM: $settingsVM)) {
+                NavigationLink(destination: GameplayView(settingsVM: $settingsVM,
+                                                         leaderboardVM: $leaderboardVM)) {
                     Text("Play")
                         .frame(width: 200, height: 50)
                         .background(Color.pink.opacity(0.5))
