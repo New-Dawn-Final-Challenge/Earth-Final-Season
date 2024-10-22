0c3c08814d5984414491e02f8bb68f190c4926da
larissafzl <fazolinlarissa@gmail.com>
2024-10-22 19:30:10 -0300

    add character screen hi-fi

--- Earth_Final_Season/Earth_Final_Season/Components/Views/CharacterView.swift
+++ Earth_Final_Season/Earth_Final_Season/Components/Views/CharacterView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Design_System
 
 struct CharacterView: View {
     let characterImage: String
@@ -21,32 +22,31 @@ struct CharacterView: View {
     var body: some View {
         VStack(spacing: 0) {
             // Image rectangle
-            Rectangle()
-                .fill(Color.gray.opacity(0.2))
-                .frame(width: getWidth() * 0.41,
-                       height: getHeight() * 0.115)
+            Assets.Images.characterScreen.swiftUIImage
+                .resizable()
+                .scaledToFit()
                 .overlay(
-                    Image(characterImage)
-                        .resizable()
-                        .frame(width: getWidth() * 0.41,
-                               height: getHeight() * 0.115)
-                        .aspectRatio(contentMode: .fill)
-                        .foregroundColor(.accentColor)
-                )
-            
-            // Name rectangle
-            Rectangle()
-                .foregroundStyle(Color(UIColor.systemGray4))
-                .frame(width: getWidth() * 0.41,
-                       height: getHeight() * 0.063)
-                .overlay(
-                    Text(characterName)
-                        .font(.bodyFont)
-                        .bold()
-                        .multilineTextAlignment(.center)
+                    VStack(spacing: 12) {
+                        Image(characterImage)
+                            .resizable()
+                            .frame(width: getWidth() * 0.4,
+                                   height: getHeight() * 0.115)
+                            .aspectRatio(contentMode: .fill)
+                            .foregroundColor(.accentColor)
+                            .cornerRadius(16)
+                        
+                        Text(characterName)
+                            .font(.bodyFont)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .lineLimit(nil)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .frame(maxWidth: .infinity)
+                    }
+                    .padding(.vertical, 35)
+                    .padding(.horizontal, 20)
                 )
         }
-        .cornerRadius(16) // Add rounding to both rectangles
     }
 }
 
