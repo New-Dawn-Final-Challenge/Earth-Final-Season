5d4c40d8c1215407279267a04de7922e10ef9a6e
Ana Elisa <elisaslimao@hotmail.com>
2024-11-06 10:03:17 -0300

    fixing the monitor's width and height

--- Earth_Final_Season/Earth_Final_Season/Components/Views/CharacterView.swift
+++ Earth_Final_Season/Earth_Final_Season/Components/Views/CharacterView.swift
@@ -29,24 +29,25 @@ struct CharacterView: View {
                        height: getHeight() * Constants.CharacterView.imageFrameHeightMultiplier)
                 .overlay(
                     // Change the distance between image and text
-                    VStack {
+                    VStack (spacing: 7) {
                         GlitchContentView(trigger: $triggerChangeChannel)
                             .frame(width: getWidth() * Constants.CharacterView.glitchViewWidthMultiplier,
                                    height: getHeight() * Constants.CharacterView.glitchViewHeightMultiplier)
                             .cornerRadius(Constants.Global.cornerRadius)
-                        Spacer()
                         
                         GlitchCharacterView(trigger: $triggerChangeChannel, characterName: characterName)
-                                                    .lineLimit(nil)
-                                                    .fixedSize(horizontal: false, vertical: true)
-                                                    .frame(maxWidth: .infinity, minHeight: 20)
+                            .lineLimit(nil)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .frame(maxWidth: .infinity, minHeight: 20)
                     }
+                    
                     .onChange(of: characterName) {
                         triggerChangeChannel.toggle()
                     }
                     // Here, the image won't move if the text changes from two-lines to one-line or vice versa
                     .padding(.vertical, Constants.CharacterView.verticalPadding)
                     .padding(.horizontal)
+                    .offset(y: -10)
                 )
         }
     }
