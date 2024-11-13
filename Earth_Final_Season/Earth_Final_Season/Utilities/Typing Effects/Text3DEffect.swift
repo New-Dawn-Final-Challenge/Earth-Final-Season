b86d300543d05a0dd20228104768777ab278eaa0
larissafzl <fazolinlarissa@gmail.com>
2024-11-13 11:27:55 -0300

    add aboutUs hifi

--- Earth_Final_Season/Earth_Final_Season/Utilities/Typing Effects/Text3DEffect.swift
+++ Earth_Final_Season/Earth_Final_Season/Utilities/Typing Effects/Text3DEffect.swift
@@ -24,6 +24,7 @@ struct Text3dEffect: View {
             Text(text)
                 .foregroundStyle(.white)
         }
+        .font(.largeTitleFont)
     }
 }
 
---
+++ Earth_Final_Season/Earth_Final_Season/ViewModels/AboutUsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AboutUsViewModel.swift
+//  Earth_Final_Season
+//
+//  Created by Larissa Fazolin on 13/11/24.
+//
+
+import Foundation
+
+@Observable
+class AboutUsViewModel {
+    var isPresentedInMenu = false
+}
