daf62d172d768f2141d417203790a60f3a47fb35
Breno Harris <80071445+Breno-VH@users.noreply.github.com>
2024-11-27 10:04:51 -0300

    tela de ajuda hifi

--- Earth_Final_Season/Earth_Final_Season/Views/HelpScreen.swift
+++ Earth_Final_Season/Earth_Final_Season/Views/HelpScreen.swift
@@ -1,52 +1,38 @@
-//
-//  HelpScreen.swift
-//  Earth_Final_Season
-//
-//  Created by Breno Harris on 22/11/24.
-//
-
 import SwiftUI
 import Design_System
 
 struct HelpScreen: View {
-    var indicatorsSize: CGFloat = 50
+    @Binding var vm: SettingsViewModel
+    var indicatorsSize: CGFloat = 30
     var title = "Instructions"
+    
     var body: some View {
         ZStack {
             popUpBackground
-            VStack(spacing: 6) {
+            
+            VStack(spacing: 0) {
                 Text3dEffect(text: title)
-                    .padding()
+                    .font(.largeTitleFont)
                 
-                VStack {
-                    Text("Chaos indicators")
-                        .font(.title1Font)
-                        .fontWeight(.semibold)
-                        .foregroundStyle(.white)
-                        
-                    indicators
-                    Text("Keep them high, \nbut never to their limit.")
-                        .foregroundStyle(.white)
-                        .font(.bodyFont)
-                        .multilineTextAlignment(.center)
-                        .padding()
-                    
-                    Text3dEffect(text: "Audience indicator")
-                        .scaleEffect(0.75)
-                    
-                    audienceSection
-                    
-                    Text("Chaos indicators keep it high.\nIf it hits the bottom, you’re fired.")
-                        .foregroundStyle(.white)
-                        .font(.bodyFont)
-                        .multilineTextAlignment(.center)
-                        .padding(.top)
-                }
+                Text("Keep the show running for as many years as you can!")
+                    .foregroundStyle(.white)
+                    .font(.title3Font)
+                    .padding(.bottom, 16)
+                    .multilineTextAlignment(.center)
                 
+                indicators
+                    .padding(.vertical, 8)
+                
+                audienceSection
+                    .padding(.vertical, 8)
+                
+                resumeButton
                 
                 Spacer()
             }
+            .padding()
         }
+        .padding()
     }
     
     var popUpBackground: some View {
@@ -60,72 +46,192 @@ struct HelpScreen: View {
     }
     
     var indicators: some View {
-        Group {
-            HStack {
-                Assets.Images.environmentalDegradationSimple.swiftUIImage
-                    .resizable()
-                    .frame(width: indicatorsSize, height: indicatorsSize)
-                    .aspectRatio(contentMode: .fit)
-                    .colorInvert()
-                Spacer()
-                Text("Environmental \ndegradation")
-                    .font(.title3Font)
-                    .foregroundStyle(.white)
-            }
-            .padding(.horizontal, 60)
+        ZStack {
+            Assets.Images.characterScreen.swiftUIImage
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: .infinity)
             
-            HStack {
-                Assets.Images.illbeingSimple.swiftUIImage
-                    .resizable()
-                    .frame(width: indicatorsSize, height: indicatorsSize)
-                    .aspectRatio(contentMode: .fit)
-                    .colorInvert()
-                Spacer()
-                Text("Ill-being     ")
-                    .font(.title3Font)
-                    .foregroundStyle(.white)
+            VStack(spacing: 0) {
+                HStack {
+                    Spacer()
+                    Text("Chaos indicators")
+                        .font(.title3Font)
+                        .foregroundStyle(.black)
+                    Spacer()
+                    Spacer()
+                }
+                .padding(.horizontal)
+                
+                VStack {
+                    HStack {
+                        Assets.Images.environmentalDegradationSimple.swiftUIImage
+                            .resizable()
+                            .frame(width: indicatorsSize, height: indicatorsSize)
+                            .aspectRatio(contentMode: .fit)
+                            .colorInvert()
+                            .padding(.leading, 16)
+                            .padding(.top, 4)
+                            .padding(.trailing, 8)
+                        
+                        VStack {
+                            Text("Environmental\ndegradation")
+                                .font(.footnoteFont)
+                                .foregroundStyle(.white)
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal, 10)
+                    
+                    Divider()
+                        .overlay(Assets.Colors.accentPrimary.swiftUIColor)
+                    
+                    HStack {
+                        Assets.Images.illbeingSimple.swiftUIImage
+                            .resizable()
+                            .frame(width: indicatorsSize, height: indicatorsSize)
+                            .aspectRatio(contentMode: .fit)
+                            .colorInvert()
+                            .padding(.leading, 16)
+                            .padding(.top, 4)
+                            .padding(.bottom, 4)
+                            .padding(.trailing, 8)
+                        
+                        VStack {
+                            Text("Ill-being")
+                                .font(.footnoteFont)
+                                .foregroundStyle(.white)
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal, 10)
+                    
+                    Divider()
+                        .overlay(Assets.Colors.accentPrimary.swiftUIColor)
+                    
+                    HStack {
+                        Assets.Images.socioPoliticalInstabilitySimple.swiftUIImage
+                            .resizable()
+                            .frame(width: indicatorsSize, height: indicatorsSize)
+                            .aspectRatio(contentMode: .fit)
+                            .colorInvert()
+                            .padding(.leading, 16)
+                            .padding(.bottom, 8)
+                            .padding(.trailing, 8)
+                        
+                        VStack {
+                            Text("Sociopolitical\ninstability")
+                                .font(.footnoteFont)
+                                .foregroundStyle(.white)
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal, 10)
+                    
+                    
+                    
+                }
+                .background(
+                    Rectangle()
+                        .fill(Assets.Colors.bgFillPrimary.swiftUIColor)
+                        .clipShape(.rect(cornerRadius: Constants.Global.cornerRadius))
+                )
+                .frame(maxWidth: 250)
+                
+                HStack {
+                    Text("Keep them ") +
+                    Text("high")
+                        .underline() +
+                    Text(", but never to their limit")
+                }
+                .font(.footnoteFont)
+                .foregroundStyle(.gray)
+                .padding(.horizontal, 36)
             }
-            .padding(.horizontal, 60)
-            
-            HStack {
-                Assets.Images.socioPoliticalInstabilitySimple.swiftUIImage
-                    .resizable()
-                    .frame(width: indicatorsSize, height: indicatorsSize)
-                    .aspectRatio(contentMode: .fit)
-                    .colorInvert()
-                Spacer()
-                Text("Sociopolitical\ninstability")
-                    .font(.title3Font)
-                    .foregroundStyle(.white)
-            }
-            .padding(.horizontal, 60)
         }
     }
     
     var audienceSection: some View {
-        Group {
-            Image(systemName: "hands.and.sparkles.fill")
+        ZStack {
+            Assets.Images.characterScreen.swiftUIImage
                 .resizable()
-                .foregroundStyle(.white)
-                .frame(width: 30, height: 30)
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: .infinity)
             
-            ZStack(alignment: .bottom) {
-                Rectangle()
-                    .fill(Color(UIColor.systemGray))
-                    .frame(width: getWidth() * Constants.AudienceIndicator.barWidthMultiplier,
-                           height: getHeight() * Constants.AudienceIndicator.barHeightMultiplier)
-
+            VStack(spacing: 4) {
+                HStack {
+                    Spacer()
+                    Text("Audience indicator")
+                        .font(.title3Font)
+                        .foregroundStyle(.black)
+                    Spacer()
+                    Spacer()
+                }
+                .padding(.horizontal)
                 
-                Rectangle()
-                    .fill(Assets.Colors.textSecondary.swiftUIColor)
-                    .frame(width: getWidth() * Constants.AudienceIndicator.barWidthMultiplier,
-                           height: 20)
+                VStack {
+                    Image(systemName: "hands.and.sparkles.fill")
+                        .resizable()
+                        .foregroundStyle(.white)
+                        .frame(width: 35, height: 40)
+                        .padding(.horizontal, 12)
+                        .padding(.top, 8)
+                    
+                    ZStack(alignment: .bottom) {
+                        Rectangle()
+                            .fill(.gray)
+                            .frame(width: getWidth() * Constants.AudienceIndicator.barWidthMultiplier * 1.25,
+                                   height: getHeight() * 1.25 * Constants.AudienceIndicator.barHeightMultiplier)
+                            .cornerRadius(Constants.Global.cornerRadius)
+                        
+                        Rectangle()
+                            .fill(Assets.Colors.textSecondary.swiftUIColor)
+                            .frame(width: getWidth() * 1.25 * Constants.AudienceIndicator.barWidthMultiplier,
+                                   height: 20 * 1.25)
+                            .cornerRadius(Constants.Global.cornerRadius)
+                    }
+                    .padding(.horizontal, 12)
+                    .padding(.bottom, 8)
+                }
+                .background(
+                    Rectangle()
+                        .fill(Assets.Colors.bgFillPrimary.swiftUIColor)
+                        .clipShape(.rect(cornerRadius: Constants.Global.cornerRadius))
+                )
+                .frame(maxWidth: .infinity)
+                
+                HStack {
+                    Text("Audience is high if ") +
+                    Text("chaos is high")
+                        .underline() +
+                    Text(". If it hits the bottom, ") +
+                    Text("you're fired")
+                        .underline()
+                }
+                    .font(.footnoteFont)
+                    .foregroundStyle(.gray)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 54)
             }
-            .cornerRadius(Constants.Global.cornerRadius)
         }
     }
-}
-
-#Preview {
-    HelpScreen()
+    
+    var resumeButton: some View {
+        Button {
+            self.vm.isPresentedHelp.toggle()
+        } label: {
+            Text("Resume")
+                .padding(16)
+                .foregroundStyle(.white)
+                .frame(maxWidth: .infinity)
+                .background(Assets.Colors.bgFillPrimary.swiftUIColor)
+                .clipShape(RoundedRectangle(cornerRadius: Constants.Global.cornerRadius))
+                .overlay {
+                    RoundedRectangle(cornerRadius: Constants.Global.cornerRadius)
+                        .stroke(Assets.Colors.accentPrimary.swiftUIColor, lineWidth: Constants.Global.lineWidth)
+                }
+        }
+        .padding(.horizontal, 30)
+        .font(.bodyFont)
+    }
 }
