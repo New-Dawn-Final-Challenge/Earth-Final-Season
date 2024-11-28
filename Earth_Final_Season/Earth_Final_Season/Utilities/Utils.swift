1eb5b2d5ae8449e02c76fc9102cc29ddaa86092b
larissafzl <fazolinlarissa@gmail.com>
2024-11-28 13:21:27 -0300

    fix bug

--- Earth_Final_Season/Earth_Final_Season/Utilities/Utils.swift
+++ Earth_Final_Season/Earth_Final_Season/Utilities/Utils.swift
@@ -40,6 +40,7 @@ struct Utils {
             case "ultra new age environmentalist": return Assets.Images.ultraNewAgeEnvironmentalist.swiftUIImage
             case "fearless economist": return Assets.Images.fearlessEconomist.swiftUIImage
             case "apocalyptical cat": return Assets.Images.apocalypticalCat.swiftUIImage
+            case "special final apocalyptical cat": return Assets.Images.specialFinalApocalypiticalCat.swiftUIImage
             case "chronically online teenager": return Assets.Images.chronicallyOnlineTeenager.swiftUIImage
             case "conspiracy theorist": return Assets.Images.conspiracyTheoristPodcaster.swiftUIImage
             case "chaotic billionaire": return Assets.Images.chaocticBillionaire.swiftUIImage
@@ -49,6 +50,7 @@ struct Utils {
             case "president in denial": return Assets.Images.presidentInDenial.swiftUIImage
             case "questionable religious leader": return Assets.Images.questionableReligiousLeader.swiftUIImage
             case "robot vacuum cleaner": return Assets.Images.robotVacumCleaner.swiftUIImage
+            case "special final robot vacuum cleaner": return Assets.Images.specialFinalRobotVaccum.swiftUIImage
             case "sensationalist tv host": return Assets.Images.sensionalistTVHost.swiftUIImage
             case "slow logistics engineer": return Assets.Images.slowLogisticEngineer.swiftUIImage
             default: return Image("image1")
